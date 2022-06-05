fa2625223358116d069a7037036c42843fb765c2
alif <alifianfajarpratama@gmail.com>
2022-06-05 11:29:21 +0700

    Update Routes

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,26 @@
-const { addNoteHandler, getAllNotesHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler } = require("./handler");
 
 const routes = [
-    {
-        method: 'POST',
-        path: '/notes',
-        handler: addNoteHandler,
-        options: {
-            cors:{
-                origin:['*'],
-            },
-        },
+  {
+    method: "POST",
+    path: "/notes",
+    handler: addNoteHandler,
+    options: {
+      cors: {
+        origin: ["*"],
+      },
     },
-    {
-        method: 'POST',
-        path: '/notes',
-        handler: getAllNotesHandler,
-        options: {
-            cors:{
-                origin:['*'],
-            },
-        },
-    }
+  },
+  {
+    method: "GET",
+    path: "/notes",
+    handler: getAllNotesHandler,
+    options: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  },
 ];
 
 module.exports = routes;
